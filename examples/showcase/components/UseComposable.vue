f22310490cbda64e4327e2203cdcfe33d5f3dc0c
devLaboware <devops@laboware.com>
2025-06-15 03:55:27 -0300

    refactor: Update package.json files and pnpm-workspace.yaml, remove old dirs

--- examples/showcase/components/UseComposable.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-const { config } = useMyComposable()
-</script>
-
-<template>
-  <h1>Using composable</h1>
-  <h5>runtime config :</h5>
-  <pre>{{ config }}</pre>
-</template>
--- examples/showcase/components/globalComp.global.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>Global Component</h1>
-</template>
